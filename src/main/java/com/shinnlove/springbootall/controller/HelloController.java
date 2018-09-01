dc2e51a4f92c020b902a5072c439fde307e1bc37
chensheng.zcs <chensheng.zcs@alipay.com>
2018-09-01 23:20:52 +0800

    初始化spring boot工程的logback日志和一些基础错误、日志类。

--- src/main/java/com/shinnlove/springbootall/controller/HelloController.java
+++ src/main/java/com/shinnlove/springbootall/controller/HelloController.java
@@ -4,9 +4,15 @@
  */
 package com.shinnlove.springbootall.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.shinnlove.springbootall.util.code.SystemResultCode;
+import com.shinnlove.springbootall.util.exception.SystemException;
+import com.shinnlove.springbootall.util.log.LoggerUtil;
 
 /**
  * Spring boot hello demo.
@@ -18,9 +24,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(value = "/hello")
 public class HelloController {
 
+    /** 默认日志门面 */
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
     @ResponseBody
     @RequestMapping(value = "/spring", produces = { "application/json;charset=UTF-8" })
     public String sayHello() {
+        LoggerUtil.info(LOGGER, "欢迎来到spring boot，这是info日志。");
+        LoggerUtil.warn(LOGGER, "欢迎来到spring boot，这是warn日志。");
+        LoggerUtil.error(LOGGER, new SystemException(SystemResultCode.SYSTEM_ERROR),
+            "欢迎来到spring boot，这是自定义系统错误日志。");
         return "Hello, spring boot.";
     }
 
