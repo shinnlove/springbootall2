088b23d675e63affa23666ee76fc336c930eb9d5
chensheng.zcs <chensheng.zcs@alipay.com>
2018-09-02 16:08:33 +0800

    spring boot 接入 activemq。

---
+++ src/main/java/com/shinnlove/springbootall/model/Msg.java
@@ -0,0 +1,12 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2018 All Rights Reserved.
+ */
+package com.shinnlove.springbootall.model;
+
+/**
+ * @author shinnlove.jinsheng
+ * @version $Id: Msg.java, v 0.1 2018-09-02 下午3:31 shinnlove.jinsheng Exp $$
+ */
+public class Msg {
+}
