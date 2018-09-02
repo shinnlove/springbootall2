053852bacf75f88fd3865f92e46351ad5fc1b66e
chensheng.zcs <chensheng.zcs@alipay.com>
2018-09-02 21:47:24 +0800

    spring boot接入自定义起步依赖。

--- src/main/java/com/shinnlove/springbootall/controller/HelloController.java
+++ src/main/java/com/shinnlove/springbootall/controller/HelloController.java
@@ -6,12 +6,14 @@ package com.shinnlove.springbootall.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.shinnlove.spring_boot_starter_hello.HelloService;
 import com.shinnlove.springbootall.util.code.SystemResultCode;
 import com.shinnlove.springbootall.util.exception.SystemException;
 import com.shinnlove.springbootall.util.log.LoggerUtil;
@@ -29,6 +31,10 @@ public class HelloController {
     /** 默认日志门面 */
     private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
 
+    /** hello服务 */
+    @Autowired
+    private HelloService        helloService;
+
     /**
      * 欢迎页，并使用logback打日志。
      *
@@ -41,7 +47,7 @@ public class HelloController {
         LoggerUtil.warn(LOGGER, "欢迎来到spring boot，这是warn日志。");
         LoggerUtil.error(LOGGER, new SystemException(SystemResultCode.SYSTEM_ERROR),
             "欢迎来到spring boot，这是自定义系统错误日志。");
-        return "Hello, spring boot.";
+        return "Hello, spring boot, welcome " + helloService.sayHello() + ".";
     }
 
     /**
